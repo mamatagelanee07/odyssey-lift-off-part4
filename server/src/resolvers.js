21f0143dba531557f5176569ad6d38e681e74c7b
mamatagelanee07 <gelaneem@email.com>
2022-04-23 20:46:34 +0100

    Added mutation to ibcrease views

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -15,8 +15,29 @@ const resolvers = {
       return dataSources.trackAPI.getModule(id);
     },
   },
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented view of track ${id}`,
+          track: track,
+        };
+      } catch (error) {
+        return {
+          code: error.extensions.response.status,
+          success: false,
+          message: error.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
+
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
+    author: ({ authorId }, { id }, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
 
